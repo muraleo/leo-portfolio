c929fdb454dcf6100882e236120ea22fe628eb54
muraleo <muraleo80923@gmail.com>
2018-11-19 16:12:38 -0500

    first deploy

---
+++ precache-manifest.57643f39823645e7d8636989abf5754c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c125d93fd71a15faaa6b8c1f14d7bdb",
+    "url": "/static/media/burger_builder-min.8c125d93.png"
+  },
+  {
+    "revision": "5f57fa4a74ceb72f3601",
+    "url": "/static/css/main.c438df78.chunk.css"
+  },
+  {
+    "revision": "3375aae0c03df248b32a00aef15fca3e",
+    "url": "/static/media/ew_checkout_4-min.3375aae0.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/static/js/1.343cbfa5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73cde5f619913be31db279f66dd93f65",
+    "url": "/static/media/ew_checkout_1-min.73cde5f6.png"
+  },
+  {
+    "revision": "ddd158cc031317b1f170c8096902ba7e",
+    "url": "/static/media/home-background-min.ddd158cc.jpg"
+  },
+  {
+    "revision": "5f57fa4a74ceb72f3601",
+    "url": "/static/js/main.5f57fa4a.chunk.js"
+  },
+  {
+    "revision": "db0c5592a5e2b9213a9c05921998b88f",
+    "url": "/static/media/omnifood-min.db0c5592.png"
+  },
+  {
+    "revision": "24834250822aa84f2e16ca873b8ccdbe",
+    "url": "/static/media/ew_landing_page-min.24834250.png"
+  },
+  {
+    "revision": "702c96faad1bf76a5a61be951bc08437",
+    "url": "/static/media/ew_login-min.702c96fa.png"
+  },
+  {
+    "revision": "24a8556f808e2d8f78fea5f46fbbdd26",
+    "url": "/static/media/ew_checkout_2-min.24a8556f.png"
+  },
+  {
+    "revision": "45594dbfefdb97fda68f5dabb834e87c",
+    "url": "/static/media/ew_checkout_3-min.45594dbf.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/static/css/1.2d641e84.chunk.css"
+  },
+  {
+    "revision": "80da8bf6e6e235394731305934a38b02",
+    "url": "/index.html"
+  }
+];
