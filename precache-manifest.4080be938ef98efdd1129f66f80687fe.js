b922637d1fa7317d825abe43490d11aec13e8496
muraleo <muraleo80923@gmail.com>
2018-11-19 21:12:40 -0500

    change carousel arrow color

---
+++ precache-manifest.4080be938ef98efdd1129f66f80687fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5c84f604b76be41fe9f93a121ebb8c",
+    "url": "/leo-portfolio/static/media/omnifood-min.2e5c84f6.png"
+  },
+  {
+    "revision": "152c7b9134cd3f110c5e",
+    "url": "/leo-portfolio/static/css/main.2453d540.chunk.css"
+  },
+  {
+    "revision": "3375aae0c03df248b32a00aef15fca3e",
+    "url": "/leo-portfolio/static/media/ew_checkout_4-min.3375aae0.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/leo-portfolio/static/js/1.343cbfa5.chunk.js"
+  },
+  {
+    "revision": "c7a8de31f916d1e670b1",
+    "url": "/leo-portfolio/static/js/runtime~main.c7a8de31.js"
+  },
+  {
+    "revision": "73cde5f619913be31db279f66dd93f65",
+    "url": "/leo-portfolio/static/media/ew_checkout_1-min.73cde5f6.png"
+  },
+  {
+    "revision": "8c125d93fd71a15faaa6b8c1f14d7bdb",
+    "url": "/leo-portfolio/static/media/burger_builder-min.8c125d93.png"
+  },
+  {
+    "revision": "152c7b9134cd3f110c5e",
+    "url": "/leo-portfolio/static/js/main.152c7b91.chunk.js"
+  },
+  {
+    "revision": "24834250822aa84f2e16ca873b8ccdbe",
+    "url": "/leo-portfolio/static/media/ew_landing_page-min.24834250.png"
+  },
+  {
+    "revision": "702c96faad1bf76a5a61be951bc08437",
+    "url": "/leo-portfolio/static/media/ew_login-min.702c96fa.png"
+  },
+  {
+    "revision": "24a8556f808e2d8f78fea5f46fbbdd26",
+    "url": "/leo-portfolio/static/media/ew_checkout_2-min.24a8556f.png"
+  },
+  {
+    "revision": "45594dbfefdb97fda68f5dabb834e87c",
+    "url": "/leo-portfolio/static/media/ew_checkout_3-min.45594dbf.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/leo-portfolio/static/css/1.2d641e84.chunk.css"
+  },
+  {
+    "revision": "0e9fee27521aa395c5d111b7c549df97",
+    "url": "/leo-portfolio/index.html"
+  }
+];
